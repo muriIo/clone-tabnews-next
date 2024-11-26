e57255a46e0307659195de48b4d132cbf9fd4d10
Murilo Cruz <murilomecr@gmail.com>
2024-11-26 19:22:18 -0300

    initializing project and adding index page

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>I hope you know that you are the most precious thing in my entire life. I'm doing everything to give you the life you deserve.</h1>;
+}
+
+export default Home;
