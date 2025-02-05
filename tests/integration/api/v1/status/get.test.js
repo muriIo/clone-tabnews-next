e9de65cbefb9ced1b89a58464bf70897c42e6cb6
Murilo Cruz <murilomecr@gmail.com>
2025-02-04 22:13:51 -0300

    returning `updated_at` in `status endpoint` and validating with tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,3 +3,19 @@ test("GET -> /api/v1/status should return 200", async () => {
 
   expect(response.status).toBe(200);
 });
+
+test("GET -> /api/v1/status should return updated_at info", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.updated_at).toBeDefined();
+});
+
+test("GET -> /api/v1/status should return updated_at info in ISO 8601 format", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+
+  expect(parsedUpdatedAt).toEqual(responseBody.updated_at);
+});
